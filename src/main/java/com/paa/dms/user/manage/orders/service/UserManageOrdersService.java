47cdb59f3e6ee0f10f4f5b2050e82de0144e705f
Hiram Sanchez <hiram.sanchz@gmail.com>
2024-10-12 15:46:49 -0700

    style: remove unused imports

--- src/main/java/com/paa/dms/user/manage/orders/service/UserManageOrdersService.java
+++ src/main/java/com/paa/dms/user/manage/orders/service/UserManageOrdersService.java
@@ -4,9 +4,6 @@ import com.paa.dms.user.manage.orders.model.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface UserManageOrdersService {
     ResponseEntity<String> saveOrder(HttpHeaders httpHeaders, RequestNewOrderEntity userRequest);
     ResponseEntity<ResponseRetrieveOrderEntity> findUserOrders(HttpHeaders httpHeaders);
